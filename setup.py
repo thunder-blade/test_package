6586d9cf7d3b3227d5d5f606e85f50e06e6254eb
thunder-blade <rit2015034@iiita.ac.in>
2019-01-14 16:51:45 +0530

    Update setup file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
-  name = 'test_package',         # How you named your package folder (MyLib)
-  packages = ['test_package'],   # Chose the same as "name"
+  name = 'A_singhal_package',         # How you named your package folder (MyLib)
+  packages = ['A_singhal_package'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Testing a package',   # Give a short description about your library
