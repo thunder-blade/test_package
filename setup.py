bea5587c47a01ed0cc361d563e1e192de5482128
thunder-blade <rit2015034@iiita.ac.in>
2019-01-14 16:30:41 +0530

    Update setup file

--- setup.py
+++ setup.py
@@ -7,12 +7,10 @@ setup(
   description = 'Testing a package',   # Give a short description about your library
   author = 'Arun Singhal',                   # Type in your name
   author_email = 'a.singhal034@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  url = 'https://github.com/thunder-blade/test_package',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/thunder-blade/test_package/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['TEST'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
